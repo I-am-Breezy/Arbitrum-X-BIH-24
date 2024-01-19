7e58e8b37201e1bae23798c59191c6296b42efe0
I-am-Breezy <156662306+I-am-Breezy@users.noreply.github.com>
2024-01-19 22:31:52 +0100

    Add files via upload
    Writing a simple arithmetic smart contract on Arbitrum Sepolia to find an odd number, an even number, and most significant bits.

---
+++ training6 (1).sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract Training6 {
+
+    function findEven(uint256 number) public pure returns(bool)
+   { 
+    return number % 2 == 0;
+   }
+
+   function findOdd (uint256 number) public pure returns(bool)
+   {
+    return number  % 2 != 0;
+   } 
+
+    function findMSB (uint256 number) public pure returns (uint256)
+    {
+        uint256 msb = number >> 255;
+        return msb ;
+    }
+    
+}
+
+    
